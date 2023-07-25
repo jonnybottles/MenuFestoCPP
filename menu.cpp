9509cf81d611e99e5157bcfc08db4a2ec7fa9d65
jonathan <xxbutler86xx@gmail.com>
2023-07-25 17:51:42 -0400

    Comment code.

--- menu.cpp
+++ menu.cpp
@@ -14,25 +14,31 @@
 
 using namespace std;
 
+// Menu constructor implementation.
+// Prints the menu name at construction.
 Menu::Menu(const string &menu_name) : menu_name(menu_name) {
     print_menu_name(menu_name);
 }
 
+// Function to clear the screen, works on Windows and Linux.
 void Menu::clear_screen(void) {
 #ifdef _WIN32
-    system("cls"); // For Windows
+    // For Windows
+    system("cls");
 #else
-    cout << "\033[2J\033[1;1H"; // ANSI escape code to clear the screen
+    // ANSI escape code to clear the screen for non-Windows systems
+    cout << "\033[2J\033[1;1H";
 #endif
 }
 
+// Prints a given menu name.
 void Menu::print_menu_name(const string &menu_name) {
-
     cout << "***** " << menu_name << " *****\n";
 }
 
+// Obtains a string from the user.
 string Menu::get_string(const string &prompt) {
-    // Register signal handler for SIGINT
+    // Register signal handler for SIGINT (Ctrl+C)
     signal(SIGINT, quit_program);
 
     string response;
@@ -40,64 +46,73 @@ string Menu::get_string(const string &prompt) {
         cout << prompt;
         // Check to see if the input stream is still good.
         if (!getline(cin, response)) {
-            // If its not good and we have reached eof (e.g. ctrl-d / ctrl-c)
+            // If we have reached EOF (e.g., Ctrl+D / Ctrl+C).
             if (cin.eof()) {
                 quit_program(EOF);
-                // Otherwise if there are any other input stream errors, quit the program.
             } else {
+                // If there are any other input stream errors, quit the program.
                 quit_program(1);
             }
         } else {
+            // Input stream is good, break the loop.
             break;
         }
     }
     return response;
 }
 
+// Helper function to validate if a string can be converted to an int
 pair<bool, int> Menu::is_valid_int(const string &response) {
     try {
+        // Try to convert the string to an integer.
+        // If successful, return true and the integer.
         return {true, stoi(response)};
     } catch (invalid_argument &e) {
+        // If a std::invalid_argument exception is caught, return false and 0.
         return {false, 0};
     }
 }
 
+// Function to get an integer input from the user.
 int Menu::get_int(const string &prompt) {
-
-    // Register signal handler for SIGINT
+    // Register signal handler for SIGINT (Ctrl+C)
     signal(SIGINT, quit_program);
 
     string response;
-    int num;
     while (true) {
         cout << prompt;
         // Check to see if the input stream is still good.
         if (!getline(cin, response)) {
-            // If its not good and we have reached eof (e.g. ctrl-d / ctrl-c)
+            // If we have reached EOF (e.g., Ctrl+D / Ctrl+C)
             if (cin.eof()) {
                 quit_program(EOF);
-                // Otherwise if there are any other input stream errors, quit the program.
             } else {
+                // If there are any other input stream errors, quit the program.
                 quit_program(1);
             }
         } else {
             pair<bool, int> parsed = is_valid_int(response);
             if (parsed.first) {
+                // If the string can be parsed to an integer, return the integer
                 return parsed.second;
             } else {
+                // If the string can't be parsed to an integer, print an error message
                 cout << "Please enter a valid number: \n";
             }
         }
     }
-    return num;
 }
 
+// Function to handle program termination
 void Menu::quit_program(int signal) {
     if (signal == SIGINT || signal == EOF) {
+        // If the program is terminated by SIGINT (Ctrl+C) or EOF (Ctrl+D), print a message
         cout << "\nExiting program.\n";
     } else {
+        // If the program is terminated due to an error, print an error message
         cerr << "Input error. Exiting program." << std::endl;
     }
 
+    // Terminate the program with the provided signal code
     exit(signal);
 }
--- menu.h
+++ menu.h
@@ -5,27 +5,35 @@
 
 using namespace std;
 
+// The Menu class provides the functions and attributes required to
+// create menus, accept, and validate user input.
 class Menu {
   public:
+    // Constructor that accepts a string for the menu name.
     Menu(const string &menu_name);
 
+    // Member functions
+
+    // Prints a given menu name.
     void print_menu_name(const string &menu_name);
+
+    // clear the screen
     void clear_screen(void);
+
+    // get a string from the user
     string get_string(const string &prompt);
+    // get an integer from the user
     int get_int(const string &prompt);
 
+    // Static function to handle program quit
     static void quit_program(int signal);
 
-    // This is a pure virtual function. This is not implemented in the Menu class.
-    // With pure virtual functions this forces all child classes of a class to
-    // override the virtual function and provide it' own implementation.
-    // THIS CANT BE IN THE MENU LIB OR ELSE IT MAKES THIS AN ABSTRACT CLASS.
-    // virtual void start() = 0;
-
   private:
+    // Private data member for the menu name
     string menu_name;
 
+    // Helper function to validate integer input
     pair<bool, int> is_valid_int(const string &response);
 };
 
-#endif
+#endif // MENU_H
